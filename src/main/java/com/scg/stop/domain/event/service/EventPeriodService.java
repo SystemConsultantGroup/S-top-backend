79934f258b83d0cc28fd1fa696bada6bed3b7bbe
yesjuhee <juhee0924@g.skku.edu>
2024-07-20 23:47:40 +0900

    feat: EventPeriodService 구현

---
+++ src/main/java/com/scg/stop/domain/event/service/EventPeriodService.java
@@ -0,0 +1,23 @@
+package com.scg.stop.domain.event.service;
+
+import com.scg.stop.domain.event.domain.EventPeriod;
+import com.scg.stop.domain.event.dto.EventPeriodDto;
+import com.scg.stop.domain.event.repository.EventPeriodRepository;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class EventPeriodService {
+
+    private final EventPeriodRepository eventPeriodRepository;
+
+    @Transactional
+    public EventPeriodDto.Response createEventPeriod(EventPeriodDto.Request requestDto) {
+        EventPeriod newEventPeriod = eventPeriodRepository.save(requestDto.toEntity());
+        return new EventPeriodDto.Response(newEventPeriod);
+    }
+
+}
