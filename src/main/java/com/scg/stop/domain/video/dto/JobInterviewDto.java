08778eedd4aaca88e97354d14a9814f94a71ca26
Hyunjun Yang <iam@2tle.io>
2024-07-22 13:49:46 +0900

    fix: change string check annotation to NotBlank

--- src/main/java/com/scg/stop/domain/video/dto/JobInterviewDto.java
+++ src/main/java/com/scg/stop/domain/video/dto/JobInterviewDto.java
@@ -2,6 +2,7 @@ package com.scg.stop.domain.video.dto;
 
 import com.scg.stop.domain.video.domain.Category;
 import com.scg.stop.domain.video.domain.JobInterview;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -15,16 +16,16 @@ public class JobInterviewDto {
     @Builder
     public static class Request {
 
-        @NotNull(message = "제목을 입력해주세요.")
+        @NotBlank(message = "제목을 입력해주세요.")
         private String title;
 
-        @NotNull(message = "유튜브 URL의 ID를 입력해주세요.")
+        @NotBlank(message = "유튜브 URL의 ID를 입력해주세요.")
         private String youtubeId;
 
         @NotNull(message = "연도를 입력해주세요.")
         private Integer year;
 
-        @NotNull(message = "카테고리를 입력해주세요.")
+        @NotBlank(message = "카테고리를 입력해주세요.")
         private Category category;
 
         public JobInterview toEntity() {
