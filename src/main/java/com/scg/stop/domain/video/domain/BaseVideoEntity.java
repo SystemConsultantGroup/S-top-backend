d20d4a4ff4ad2d508a137142c2cacac1777acc4c
Hyunjun Yang <iam@2tle.io>
2024-07-22 13:41:58 +0900

    fix: change BaseVideoEntity access level

--- src/main/java/com/scg/stop/domain/video/domain/BaseVideoEntity.java
+++ src/main/java/com/scg/stop/domain/video/domain/BaseVideoEntity.java
@@ -7,13 +7,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
 public class BaseVideoEntity extends BaseTimeEntity {
 
