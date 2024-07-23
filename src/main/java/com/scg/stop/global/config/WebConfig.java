abc80b887032d2fb769018f10ee2a5f5622fd494
chanyeong <jcy030896@naver.com>
2024-07-23 20:28:57 +0900

    feat: webconfig 작성

--- src/main/java/com/scg/stop/global/config/WebConfig.java
+++ src/main/java/com/scg/stop/global/config/WebConfig.java
@@ -1,2 +1,29 @@
-package com.scg.stop.global.config;public class WebConfig {
+package com.scg.stop.global.config;
+
+import com.scg.stop.auth.config.AuthUserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(authUserArgumentResolver);
+    }
 }
