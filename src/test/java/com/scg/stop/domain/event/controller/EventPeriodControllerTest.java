12f5eef81e7eff5b158e295a2e71b65438dbd31b
yesjuhee <juhee0924@g.skku.edu>
2024-07-26 01:13:02 +0900

    feat: getEventPeriods API 테스트, restdocs 구현

--- src/test/java/com/scg/stop/domain/event/controller/EventPeriodControllerTest.java
+++ src/test/java/com/scg/stop/domain/event/controller/EventPeriodControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,6 +17,9 @@ import com.scg.stop.domain.event.dto.request.CreateEventPeriodRequest;
 import com.scg.stop.domain.event.dto.response.EventPeriodResponse;
 import com.scg.stop.domain.event.service.EventPeriodService;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +45,6 @@ class EventPeriodControllerTest extends AbstractControllerTest {
     @Test
     @DisplayName("이벤트 기간을 생성할 수 있다.")
     void createEventPeriod() throws Exception {
-
         // given
         CreateEventPeriodRequest request = new CreateEventPeriodRequest(2024, LocalDateTime.now(), LocalDateTime.now().plusDays(10));
         EventPeriodResponse response = new EventPeriodResponse(1L, 2024, LocalDateTime.now(), LocalDateTime.now().plusDays(10), LocalDateTime.now(), LocalDateTime.now());
@@ -74,10 +77,9 @@ class EventPeriodControllerTest extends AbstractControllerTest {
                 ));
     }
 
-    @DisplayName("이벤트 연도를 입력하지 않으면 예외가 발생한다.")
     @Test
+    @DisplayName("이벤트 연도를 입력하지 않으면 예외가 발생한다.")
     void createEventPeriodWithInvalidYear() throws Exception {
-
         // given
         CreateEventPeriodRequest request = new CreateEventPeriodRequest( null, LocalDateTime.now(), LocalDateTime.now().plusDays(10));
 
@@ -90,5 +92,33 @@ class EventPeriodControllerTest extends AbstractControllerTest {
 
         // then
         result.andExpect(status().isBadRequest());
+    }
+
+    // TODO Auth 설정 추가
+    @Test
+    @DisplayName("전체 이벤트 기간 설정을 조회할 수 있다.")
+    void getEventPeriods() throws Exception {
+        // given
+        List<EventPeriodResponse> responses = Arrays.asList(
+                new EventPeriodResponse(1L, 2024, LocalDateTime.now(), LocalDateTime.now().plusDays(10), LocalDateTime.now(), LocalDateTime.now()),
+                new EventPeriodResponse(2L, 2025, LocalDateTime.now(), LocalDateTime.now().plusDays(10), LocalDateTime.now(), LocalDateTime.now())
+        );
+        when(eventPeriodService.getEventPeriods()).thenReturn(responses);
+
+        // when
+        ResultActions result = mockMvc.perform(get("/eventPeriods").contentType(APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        responseFields(
+                                fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("행사 기간 ID"),
+                                fieldWithPath("[].year").type(JsonFieldType.NUMBER).description("이벤트 연도"),
+                                fieldWithPath("[].start").type(JsonFieldType.STRING).description("이벤트 시작 일시"),
+                                fieldWithPath("[].end").type(JsonFieldType.STRING).description("이벤트 종료 일시"),
+                                fieldWithPath("[].createdAt").type(JsonFieldType.STRING).description("생성일"),
+                                fieldWithPath("[].updatedAt").type(JsonFieldType.STRING).description("변경일")
+                        )
+                ));
     }
 }
