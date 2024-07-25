686cd19f6b1812e20f60d4b8a426a97e17a3f46e
yesjuhee <juhee0924@g.skku.edu>
2024-07-26 00:55:12 +0900

    feat: getEvnetPeriods api 구현

--- src/main/java/com/scg/stop/domain/event/controller/EventPeriodController.java
+++ src/main/java/com/scg/stop/domain/event/controller/EventPeriodController.java
@@ -1,12 +1,14 @@
 package com.scg.stop.domain.event.controller;
 
-import com.scg.stop.domain.event.dto.EventPeriodRequest;
-import com.scg.stop.domain.event.dto.EventPeriodResponse;
+import com.scg.stop.domain.event.dto.request.CreateEventPeriodRequest;
+import com.scg.stop.domain.event.dto.response.EventPeriodResponse;
 import com.scg.stop.domain.event.service.EventPeriodService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,15 @@ public class EventPeriodController {
 
     // TODO : ADMIN 권한 확인
     @PostMapping
-    public ResponseEntity<EventPeriodResponse> createEventPeriod(@RequestBody @Valid EventPeriodRequest createEventPeriodRequest) {
+    public ResponseEntity<EventPeriodResponse> createEventPeriod(@RequestBody @Valid CreateEventPeriodRequest createEventPeriodRequest) {
         EventPeriodResponse eventPeriodResponse = eventPeriodService.createEventPeriod(createEventPeriodRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(eventPeriodResponse);
+    }
+
+    // TODO : ADMIN 권한 확인
+    @GetMapping
+    public ResponseEntity<List<EventPeriodResponse>> getEventPeriods() {
+        List<EventPeriodResponse> eventPeriodResponses = eventPeriodService.getEventPeriods();
+        return ResponseEntity.ok().body(eventPeriodResponses);
     }
 }
--- src/main/java/com/scg/stop/domain/event/service/EventPeriodService.java
+++ src/main/java/com/scg/stop/domain/event/service/EventPeriodService.java
@@ -1,12 +1,15 @@
 package com.scg.stop.domain.event.service;
 
 import static com.scg.stop.global.exception.ExceptionCode.DUPLICATED_YEAR;
+import static org.hamcrest.Matchers.any;
 
 import com.scg.stop.domain.event.domain.EventPeriod;
-import com.scg.stop.domain.event.dto.EventPeriodRequest;
-import com.scg.stop.domain.event.dto.EventPeriodResponse;
+import com.scg.stop.domain.event.dto.request.CreateEventPeriodRequest;
+import com.scg.stop.domain.event.dto.response.EventPeriodResponse;
 import com.scg.stop.domain.event.repository.EventPeriodRepository;
 import com.scg.stop.global.exception.BadRequestException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,11 +21,20 @@ public class EventPeriodService {
 
     private final EventPeriodRepository eventPeriodRepository;
 
-    public EventPeriodResponse createEventPeriod(EventPeriodRequest createEventPeriodRequest) {
+    public EventPeriodResponse createEventPeriod(CreateEventPeriodRequest createEventPeriodRequest) {
         if (eventPeriodRepository.existsByYear(createEventPeriodRequest.getYear())) {
             throw new BadRequestException(DUPLICATED_YEAR);
         }
         EventPeriod newEventPeriod = eventPeriodRepository.save(EventPeriod.from(createEventPeriodRequest));
         return EventPeriodResponse.from(newEventPeriod);
+    }
+
+    @Transactional(readOnly = true)
+    public List<EventPeriodResponse> getEventPeriods() {
+        List<EventPeriod> eventPeriods = eventPeriodRepository.findAll();
+        List<EventPeriodResponse> eventPeriodResponses = eventPeriods.stream()
+                .map(EventPeriodResponse::from)
+                .collect(Collectors.toList());
+        return eventPeriodResponses;
     }
 }
