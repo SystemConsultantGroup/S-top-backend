2975acbb10630eb815a52a8d6737051f0765d41b
chanyeong <jcy030896@naver.com>
2024-07-26 12:18:40 +0900

    feat: register dto 작성

--- src/main/java/com/scg/stop/auth/domain/request/RegisterRequest.java
+++ src/main/java/com/scg/stop/auth/domain/request/RegisterRequest.java
@@ -1,6 +1,9 @@
 package com.scg.stop.auth.domain.request;
 
+import com.scg.stop.user.domain.Student;
 import com.scg.stop.user.domain.UserType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,8 +11,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;
+
+    @NotBlank(message = "전화번호를 입력해주세요.")
     private String phoneNumber;
+
+    @NotBlank(message = "유저유형을 입력해주세요.")
     private UserType userType;
 
+    @NotBlank(message = "이메일을 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "가입 경로를 입력해주세요")
+    private String signUpSource;
+
+    private Student studentInfo;
 }
