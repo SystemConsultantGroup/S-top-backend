be3f16fac164bec26d0a2661e5c53f8b4ec070a0
yeobi01 <yeobi_01@naver.com>
2024-07-26 17:27:33 +0900

    feat: add createProject controller

--- src/main/java/com/scg/stop/domain/project/controller/ProjectController.java
+++ src/main/java/com/scg/stop/domain/project/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package com.scg.stop.domain.project.controller;
 
+import com.scg.stop.domain.project.dto.request.ProjectRequest;
 import com.scg.stop.domain.project.service.ProjectService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectController {
 
     private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<Void> createProject(
+            @RequestBody @Valid ProjectRequest projectRequest
+    ) {
+        projectService.createProject(projectRequest);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
