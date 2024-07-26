a9b44c08c3dbdc77f20528ca7c46d7a773389d5e
yesjuhee <juhee0924@g.skku.edu>
2024-07-26 16:34:33 +0900

    [FIX] 도메인 수정 (#26)
    * fix: BaseNoticeEntity 삭제
    
    * fix: BaseVideoEntity 삭제
    
    * fix: BaseRespnseEntity 삭제
    
    * fix: Project videoUrl -> youtubeId 변경
    
    * fix: UserType TEMP 추가

--- src/main/java/com/scg/stop/domain/project/domain/InquiryResponse.java
+++ src/main/java/com/scg/stop/domain/project/domain/InquiryResponse.java
@@ -1,10 +1,14 @@
 package com.scg.stop.domain.project.domain;
 
 import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-import com.scg.stop.global.domain.BaseResponseEntity;
+import com.scg.stop.global.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
@@ -13,7 +17,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-public class InquiryResponse extends BaseResponseEntity {
+public class InquiryResponse extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
 
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "inquiry_id")
