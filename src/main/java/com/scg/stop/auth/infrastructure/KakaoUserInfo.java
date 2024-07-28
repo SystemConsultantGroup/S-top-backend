999380b7f942986f22447e0a199f7daefe2542d7
chanyeong <jcy030896@naver.com>
2024-07-28 22:43:16 +0900

    fix: 카카오에서 닉네임 받지 않도록 수정

--- src/main/java/com/scg/stop/auth/infrastructure/KakaoUserInfo.java
+++ src/main/java/com/scg/stop/auth/infrastructure/KakaoUserInfo.java
@@ -13,17 +13,10 @@ public class KakaoUserInfo {
     @JsonProperty("kakao_account")
     private KakaoAccount kakaoAccount;
 
-    public String getNickname() {
-        return kakaoAccount.kakaoProfile.nickname;
-    }
-
     private static class KakaoAccount {
         @JsonProperty("profile")
         private KakaoProfile kakaoProfile;
     }
 
-    private static class KakaoProfile {
-        @JsonProperty("nickname")
-        private String nickname;
-    }
+    private static class KakaoProfile {}
 }
