714c1c023e01884ec1daef38b3bb2faf811a82e8
chanyeong <jcy030896@naver.com>
2024-07-30 19:09:57 +0900

    refactor: oauth 인증방식 변경

--- src/main/java/com/scg/stop/auth/controller/AuthController.java
+++ src/main/java/com/scg/stop/auth/controller/AuthController.java
@@ -17,11 +17,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,9 +33,9 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping(value = "/login/kakao/{accessCode}")
+    @PostMapping(value = "/login/kakao")
     public ResponseEntity<AccessTokenResponse> kakaoLogin(
-            @PathVariable("accessCode") String accessCode ,
+            @RequestParam("code") String accessCode ,
             HttpServletResponse response
     ) {
         UserToken userTokens = authService.login(accessCode);
