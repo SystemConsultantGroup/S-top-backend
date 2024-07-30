e3a91211d489e4d72ab9b1ddd63a086edf27c270
yeobi01 <yeobi_01@naver.com>
2024-07-30 11:40:10 +0900

    feat: projectController 작성

--- src/main/java/com/scg/stop/domain/project/controller/ProjectController.java
+++ src/main/java/com/scg/stop/domain/project/controller/ProjectController.java
@@ -1,14 +1,13 @@
 package com.scg.stop.domain.project.controller;
 
 import com.scg.stop.domain.project.dto.request.ProjectRequest;
+import com.scg.stop.domain.project.dto.response.ProjectDetailResponse;
 import com.scg.stop.domain.project.service.ProjectService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,12 +17,35 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping
-    public ResponseEntity<Void> createProject(
+    public ResponseEntity<ProjectDetailResponse> createProject(
             @RequestBody @Valid ProjectRequest projectRequest
     ) {
-        projectService.createProject(projectRequest);
-        return ResponseEntity.ok().build();
+        ProjectDetailResponse projectDetailResponse = projectService.createProject(projectRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectDetailResponse);
     }
 
+    @GetMapping("/{projectId}")
+    public ResponseEntity<ProjectDetailResponse> getProject(
+            @PathVariable Long projectId
+    ) {
+        ProjectDetailResponse projectDetailResponse = projectService.getProject(projectId);
+        return ResponseEntity.status(HttpStatus.OK).body(projectDetailResponse);
+    }
 
+    @PutMapping("/{projectId}")
+    public ResponseEntity<ProjectDetailResponse> updateProject(
+            @PathVariable Long projectId,
+            @RequestBody @Valid ProjectRequest projectRequest
+    ) {
+        ProjectDetailResponse projectDetailResponse = projectService.updateProject(projectId, projectRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(projectDetailResponse);
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<Void> deleteProject(
+            @PathVariable Long projectId
+    ) {
+        projectService.deleteProject(projectId);
+        return ResponseEntity.noContent().build();
+    }
 }
