be8d29ed2b6e0dd437245f98eeca468fc1fc6bb3
yesjuhee <juhee0924@g.skku.edu>
2024-08-03 19:52:33 +0900

    fix: test, restdocs 수정

--- src/test/java/com/scg/stop/event/controller/EventPeriodControllerTest.java
+++ src/test/java/com/scg/stop/event/controller/EventPeriodControllerTest.java
@@ -1,4 +1,4 @@
-package com.scg.stop.domain.event.controller;
+package com.scg.stop.event.controller;
 
 
 import static org.mockito.ArgumentMatchers.any;
@@ -9,6 +9,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -51,8 +52,7 @@ class EventPeriodControllerTest extends AbstractControllerTest {
     @DisplayName("이벤트 기간을 생성할 수 있다.")
     void createEventPeriod() throws Exception {
         // given
-        EventPeriodRequest request = new EventPeriodRequest(2024, LocalDateTime.now(),
-                LocalDateTime.now().plusDays(10));
+        EventPeriodRequest request = new EventPeriodRequest(LocalDateTime.now(), LocalDateTime.now().plusDays(10));
         EventPeriodResponse response = new EventPeriodResponse(1L, 2024, LocalDateTime.now(),
                 LocalDateTime.now().plusDays(10), LocalDateTime.now(), LocalDateTime.now());
 
@@ -69,12 +69,11 @@ class EventPeriodControllerTest extends AbstractControllerTest {
         result.andExpect(status().isCreated())
                 .andDo(restDocs.document(
                         requestFields(
-                                fieldWithPath("year").type(JsonFieldType.NUMBER).description("이벤트 연도"),
                                 fieldWithPath("start").type(JsonFieldType.STRING).description("이벤트 시작 일시"),
                                 fieldWithPath("end").type(JsonFieldType.STRING).description("이벤트 종료 일시")
                         ),
                         responseFields(
-                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("행사 기간 ID"),
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("이벤트 기간 ID"),
                                 fieldWithPath("year").type(JsonFieldType.NUMBER).description("이벤트 연도"),
                                 fieldWithPath("start").type(JsonFieldType.STRING).description("이벤트 시작 일시"),
                                 fieldWithPath("end").type(JsonFieldType.STRING).description("이벤트 종료 일시"),
@@ -84,67 +83,82 @@ class EventPeriodControllerTest extends AbstractControllerTest {
                 ));
     }
 
-    @Test
-    @DisplayName("이벤트 연도를 입력하지 않으면 예외가 발생한다.")
-    void createEventPeriodWithInvalidYear() throws Exception {
-        // given
-        EventPeriodRequest request = new EventPeriodRequest(null, LocalDateTime.now(),
-                LocalDateTime.now().plusDays(10));
-
-        // when
-        ResultActions result = mockMvc.perform(
-                post("/eventPeriods")
-                        .contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request))
-        );
-
-        // then
-        result.andExpect(status().isBadRequest());
-    }
-
     // TODO Auth 설정 추가
     @Test
-    @DisplayName("전체 이벤트 기간 설정을 조회할 수 있다.")
-    void getEventPeriods() throws Exception {
+    @DisplayName("올해의 이벤트 기간 설정을 조회할 수 있다.")
+    void getEventPeriod() throws Exception {
         // given
-        List<EventPeriodResponse> responses = Arrays.asList(
-                new EventPeriodResponse(1L, 2024, LocalDateTime.now(), LocalDateTime.now().plusDays(10),
-                        LocalDateTime.now(), LocalDateTime.now()),
-                new EventPeriodResponse(2L, 2025, LocalDateTime.now(), LocalDateTime.now().plusDays(10),
-                        LocalDateTime.now(), LocalDateTime.now())
-        );
-        when(eventPeriodService.getEventPeriod()).thenReturn(responses);
+        EventPeriodResponse response = new EventPeriodResponse(1L, LocalDateTime.now().getYear(),
+                LocalDateTime.now(), LocalDateTime.now().plusDays(10),
+                LocalDateTime.now(), LocalDateTime.now());
+        when(eventPeriodService.getEventPeriod()).thenReturn(response);
 
         // when
-        ResultActions result = mockMvc.perform(get("/eventPeriods").contentType(APPLICATION_JSON));
+        ResultActions result = mockMvc.perform(get("/eventPeriod").contentType(APPLICATION_JSON));
 
         // then
         result.andExpect(status().isOk())
                 .andDo(restDocs.document(
                         responseFields(
-                                fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("행사 기간 ID"),
-                                fieldWithPath("[].year").type(JsonFieldType.NUMBER).description("이벤트 연도"),
-                                fieldWithPath("[].start").type(JsonFieldType.STRING).description("이벤트 시작 일시"),
-                                fieldWithPath("[].end").type(JsonFieldType.STRING).description("이벤트 종료 일시"),
-                                fieldWithPath("[].createdAt").type(JsonFieldType.STRING).description("생성일"),
-                                fieldWithPath("[].updatedAt").type(JsonFieldType.STRING).description("변경일")
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("이벤트 기간 ID"),
+                                fieldWithPath("year").type(JsonFieldType.NUMBER).description("이벤트 연도"),
+                                fieldWithPath("start").type(JsonFieldType.STRING).description("이벤트 시작 일시"),
+                                fieldWithPath("end").type(JsonFieldType.STRING).description("이벤트 종료 일시"),
+                                fieldWithPath("createdAt").type(JsonFieldType.STRING).description("생성일"),
+                                fieldWithPath("updatedAt").type(JsonFieldType.STRING).description("변경일")
                         )
                 ));
     }
 
+//    @Test
+//    @DisplayName("요청한 ID에 해당하는 행사 기간을 삭제할 수 있다.")
+//    void deleteEventPeriods() throws Exception {
+//        // given
+//        doNothing().when(eventPeriodService).deleteEventPeriod(anyLong());
+//
+//        // when
+//        ResultActions result = mockMvc.perform(delete("/eventPeriods/{eventPeriodId}", 1L).contentType(APPLICATION_JSON));
+//
+//        // then
+//        result.andExpect(status().isNoContent())
+//                .andDo(restDocs.document(
+//                        pathParameters(parameterWithName("eventPeriodId").description("삭제할 행사 기간의 ID"))
+//                ));
+//    }
+
     @Test
-    @DisplayName("요청한 ID에 해당하는 행사 기간을 삭제할 수 있다.")
-    void deleteEventPeriods() throws Exception {
+    @DisplayName("이벤트 기간을 수정할 수 있다.")
+    void updateEventPeriod() throws Exception {
         // given
-        doNothing().when(eventPeriodService).deleteEventPeriod(anyLong());
+        EventPeriodRequest request = new EventPeriodRequest(LocalDateTime.now(), LocalDateTime.now().plusDays(10));
+        EventPeriodResponse response = new EventPeriodResponse(1L, LocalDateTime.now().getYear(),
+                LocalDateTime.now(), LocalDateTime.now().plusDays(10),
+                LocalDateTime.now(), LocalDateTime.now());
+
+        when(eventPeriodService.updateEventPeriod(any(EventPeriodRequest.class))).thenReturn(response);
 
         // when
-        ResultActions result = mockMvc.perform(delete("/eventPeriods/{eventPeriodId}", 1L).contentType(APPLICATION_JSON));
+        ResultActions result = mockMvc.perform(
+                put("/eventPeriod")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        );
 
         // then
-        result.andExpect(status().isNoContent())
+        result.andExpect(status().isOk())
                 .andDo(restDocs.document(
-                        pathParameters(parameterWithName("eventPeriodId").description("삭제할 행사 기간의 ID"))
+                        requestFields(
+                                fieldWithPath("start").type(JsonFieldType.STRING).description("이벤트 시작 일시"),
+                                fieldWithPath("end").type(JsonFieldType.STRING).description("이벤트 종료 일시")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("이벤트 기간 ID"),
+                                fieldWithPath("year").type(JsonFieldType.NUMBER).description("이벤트 연도"),
+                                fieldWithPath("start").type(JsonFieldType.STRING).description("이벤트 시작 일시"),
+                                fieldWithPath("end").type(JsonFieldType.STRING).description("이벤트 종료 일시"),
+                                fieldWithPath("createdAt").type(JsonFieldType.STRING).description("생성일"),
+                                fieldWithPath("updatedAt").type(JsonFieldType.STRING).description("변경일")
+                        )
                 ));
     }
 }
