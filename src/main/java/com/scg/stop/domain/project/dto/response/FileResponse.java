2f240114cf9651322ab9b2bf230b8251feb1a629
yeobi01 <yeobi_01@naver.com>
2024-08-19 14:13:31 +0900

    add: FileResponse id 추가

--- src/main/java/com/scg/stop/domain/project/dto/response/FileResponse.java
+++ src/main/java/com/scg/stop/domain/project/dto/response/FileResponse.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class FileResponse {
+    private Long id;
     private String uuid;
     private String name;
     private String mimeType;
 
     public static FileResponse from(File file) {
         return new FileResponse(
+                file.getId(),
                 file.getUuid(),
                 file.getName(),
                 file.getMimeType()
