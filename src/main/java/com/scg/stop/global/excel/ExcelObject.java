2239006b947751fafa0afd25686e901a95a15e0e
Hyunjun Yang <sw4019@naver.com>
2024-08-23 17:01:51 +0900

    feat: add apache.poi in build.gradle

---
+++ src/main/java/com/scg/stop/global/excel/ExcelObject.java
@@ -0,0 +1,5 @@
+package com.scg.stop.global.excel;
+
+public interface ExcelObject {
+    void fillUpFromRow(Row row);
+}
---
+++ src/main/java/com/scg/stop/global/excel/util/ExcelUtil.java
@@ -0,0 +1,7 @@
+package com.scg.stop.global.excel.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExcelUtil {
+}
