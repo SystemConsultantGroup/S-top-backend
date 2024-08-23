5638d3c170ce744a19bb423099fc14a912a25d54
hyzoon <shj83811081@gmail.com>
2024-08-23 15:52:00 +0900

    fix: modify createInquiry api

--- src/main/java/com/scg/stop/project/controller/ProjectController.java
+++ src/main/java/com/scg/stop/project/controller/ProjectController.java
@@ -8,6 +8,8 @@ import com.scg.stop.user.domain.AccessType;
 import com.scg.stop.user.domain.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,12 +19,15 @@ public class ProjectController {
 
     private final ProjectService projectService;
 
+    // 문의 생성
     @PostMapping("{projectId}/inquiry")
-    public InquiryDetailResponse createProjectInquiry(
+    public ResponseEntity<InquiryDetailResponse> createProjectInquiry(
             @PathVariable Long projectId,
             @AuthUser(accessType = AccessType.COMPANY) User user,
             @RequestBody @Valid InquiryRequest inquiryRequest) {
-        return projectService.createProjectInquiry(projectId, user, inquiryRequest);
+
+        InquiryDetailResponse inquiryDetailResponse = projectService.createProjectInquiry(projectId, user, inquiryRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(inquiryDetailResponse);
 
     }
 
