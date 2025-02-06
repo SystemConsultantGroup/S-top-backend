bccca7e44cc804c86d8fe64367ad402613fb9e91
Hyunjun Yang <iam@2tle.io>
2025-02-06 13:56:38 +0900

    Update QuizService.java (#145)

--- src/main/java/com/scg/stop/video/service/QuizService.java
+++ src/main/java/com/scg/stop/video/service/QuizService.java
@@ -26,7 +26,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
