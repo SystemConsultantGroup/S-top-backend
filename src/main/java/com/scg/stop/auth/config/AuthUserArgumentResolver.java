a4cc0ca676f9b2e05392cd4248499f9b3de08ccc
chanyeong <jcy030896@naver.com>
2025-02-07 11:33:17 +0900

    fix: jwt userId추가

--- src/main/java/com/scg/stop/auth/config/AuthUserArgumentResolver.java
+++ src/main/java/com/scg/stop/auth/config/AuthUserArgumentResolver.java
@@ -122,7 +122,7 @@ public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
     }
 
     private User extractUser(String accessToken) {
-        Long userId = Long.valueOf(jwtUtil.getSubject(accessToken));
+        Long userId = Long.valueOf(jwtUtil.getSubject(accessToken).get("userId", String.class));
 
         return userRepository.findById(userId)
                 .orElseThrow(() -> new BadRequestException(ExceptionCode.NOT_FOUND_USER_ID));
