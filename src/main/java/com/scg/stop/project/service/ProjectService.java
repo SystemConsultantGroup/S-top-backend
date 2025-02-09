70bab92d7c6743524b5d8065189ea12420850696
hyzoon <shj83811081@gmail.com>
2025-02-10 00:44:04 +0900

    Revert "[FIX] 올해 프로젝트 조회시 순서 랜덤하게 응답 (#154)"
    This reverts commit 1916e39aca5f9f95b1bc440ffff6968270ba29f1.

--- src/main/java/com/scg/stop/project/service/ProjectService.java
+++ src/main/java/com/scg/stop/project/service/ProjectService.java
@@ -60,12 +60,7 @@ public class ProjectService {
 
     @Transactional(readOnly = true)
     public Page<ProjectResponse> getProjects(String title, List<Integer> year, List<ProjectCategory> category, List<ProjectType> type, Pageable pageable, User user){
-        boolean isEvent = year != null && year.size() == 1 && year.get(0) == LocalDateTime.now().getYear();
-        Integer seed = (int) Math.floor(LocalDateTime.now().getHour() / 1.0);
-        Page<Project> projects = isEvent
-                ? projectRepository.findEventProjects(title, year, category, type, seed, pageable)
-                : projectRepository.findProjects(title, year, category, type, pageable);
-
+        Page<Project> projects = projectRepository.findProjects(title, year, category, type, pageable);
         Page<ProjectResponse> projectResponses = projects.map(project -> ProjectResponse.of(user, project));
         return projectResponses;
     }
